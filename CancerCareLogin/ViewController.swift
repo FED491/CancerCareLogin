b773ff1fa2f24d7a94b24d9174c13146a6cae683
Aykut <aykut@aykut-macbook-air.local>
2016-11-04 19:23:12 +0200

    Initial Commit

---
+++ CancerCareLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CancerCareLogin
+//
+//  Created by Aykut on 4.11.2016.
+//  Copyright © 2016 FED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
